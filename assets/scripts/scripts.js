0cb9a48cf1f4fdfa267761943d80ff843dbd1926
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-03-04 23:12:50 +0200

    home page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -13,4 +13,26 @@ $(() => {
 	faqsTab.on("shown.bs.tab", () => {
 		docSlider.slick("setPosition");
 	});
+
+	const testimonialsSlider = $(".testimonials__slider");
+	if (testimonialsSlider) {
+		testimonialsSlider.slick({
+			arrows: false,
+			dots: true,
+			slidesToShow: 3,
+			slidesToPreview: 1,
+			autoplay: true,
+		});
+	}
+
+	const videosSlider = $(".videos__slider");
+	if (videosSlider) {
+		videosSlider.slick({
+			arrows: true,
+			dots: false,
+			slidesToShow: 2,
+			slidesToPreview: 2,
+			autoplay: true,
+		});
+	}
 });
