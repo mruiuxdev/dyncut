b283ba7a47966a2edd089630d0401cd2b26317d3
M-Ramadan <mr.uiux.dev@gmail.com>
2023-03-07 16:54:20 +0200

    Modification

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,10 +1,39 @@
 "use strict";
 
 $(() => {
+  $(window).on("load", function () {
+    const loader = $("#loader");
+    if (loader) {
+      $("body").css("overflow-y", "hidden");
+      loader.fadeOut("slow", function () {
+        $("body").css("overflow-y", "auto");
+      });
+    }
+  });
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  if (forms) {
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  }
+
+  const navbar = $("nav.navbar");
+  const navbarBrand = $(".navbar .navbar-brand img");
   $(window).on("scroll", function () {
     const scrollPosition = $(window).scrollTop();
-    const navbar = $("nav.navbar");
-    const navbarBrand = $(".navbar .navbar-brand img");
 
     if (scrollPosition > 100) {
       navbar.addClass("scrolled");
@@ -12,6 +41,26 @@ $(() => {
     } else {
       navbar.removeClass("scrolled");
       navbarBrand.attr("src", "/assets/images/dyncut.svg");
+    }
+  });
+
+  const btnToggle = $(".navbar .navbar-toggler");
+  if (btnToggle) {
+    btnToggle.on("click", function () {
+      if (btnToggle.attr("aria-expanded") === "true") {
+        $("body").css("overflow-y", "hidden");
+      } else {
+        $("body").css("overflow-y", "auto");
+      }
+    });
+  }
+
+  const navbarCollapse = $(".navbar .navbar-collapse");
+  $(document).on("click", function (event) {
+    if (!$(event.target).closest(".navbar,.navbar .navbar-toggler").length) {
+      navbarCollapse.removeClass("collapse show");
+      btnToggle.attr("aria-expanded", "false");
+      $("body").css("overflow-y", "auto");
     }
   });
 
@@ -92,8 +141,8 @@ $(() => {
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
-			arrows: false,
-			dots: true
+            arrows: false,
+            dots: true,
           },
         },
       ],
